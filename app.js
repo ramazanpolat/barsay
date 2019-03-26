8c2dfb08babae62ebf9fbe2f96c38534ce1072d0
Ramazan Polat <ramazanpolat@gmail.com>
2019-03-26 03:25:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = { video: { facingMode: {
+            ideal: "environment"
+        }
+  }, audio: false };
 var track = null;
 
 // Define constants
